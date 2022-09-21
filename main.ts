21627e1719848a4015df45096650b7780f306b99
strmaxxx <113727481+strmaxxx@users.noreply.github.com>
2022-09-21 14:49:19 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,100 @@
-let suma_z = 0
-let index_z = 0
-let t_pause = 50
-let avg_z_count = 5
-let pole_z:any[] = [];
+namespace NewtonovyZakony{
 
+    //% block="Spustí odeílatele skupina_rádia: %radioGroup, Osa_akcelerometru: %dimension, popis_hodnoty: %popis_hodnoty, rychlost_motoru: %rychlost_motoru, Pauza_mezi_merenimi(ms): %t_pause, Počet hodnot v klouzavém průměru: %avg_count, Rozsah měřených hodnot: %range"
+    export function spustOdesilatele(radioGroup: number, dimension: Dimension, popis_hodnoty:string, rychlost_motoru:number, t_pause?: number, avg_count?: number, range?: AcceleratorRange){
 
+        let suma = 0
+        let index = 0
+        let pole_hodnot: any[] = [];
 
-radio.setGroup(10)
-input.setAccelerometerRange(AcceleratorRange.OneG)
-control.inBackground(function () {
-    while (true) {
-        pole_z[index_z] = input.acceleration(Dimension.Z)
-        index_z += 1
-        if (index_z > avg_z_count) {
-            index_z = 0
-        }
-        suma_z = 0
-        for (let i = 0; i <= pole_z.length - 1; i++) {
-            suma_z += pole_z[i]
-        }
-        radio.sendString("z:" + Math.round(suma_z / pole_z.length))
-        //serial.writeString("z:" + suma_z / pole_z.length)
-        pause(t_pause);
+        if (!range) range = AcceleratorRange.OneG;
+        if (!t_pause) t_pause = 50;
+
+        radio.setGroup(radioGroup);
+        input.setAccelerometerRange(range)
+
+        control.inBackground(function(){
+
+            while (true) {
+                pole_hodnot[index] = input.acceleration(dimension)
+                index += 1
+                if (index > avg_count) index = 0
+
+                suma = 0
+                for (let i = 0; i < pole_hodnot.length; i++) suma += pole_hodnot[i]
+                pause(10);
+            }
+        });
+        control.inBackground(function () {
+            while (true) {
+
+                radio.sendString(popis_hodnoty + ":" + Math.round(suma / pole_hodnot.length))
+                //serial.writeLine(popis_hodnoty + ":" + suma / pole_hodnot.length)
+                pause(t_pause);
+            }
+        })
+        
+        radio.onReceivedString(function(receivedString: string) {
+            
+            if (receivedString == 'GOGO'){
+
+                wuKong.setAllMotor(rychlost_motoru, rychlost_motoru);
+
+            }
+
+            if (receivedString == 'STOP') {
+                wuKong.setAllMotor(0, 0);
+            }
+        })
+
     }
-})
+
+
+    export function spustPrijemce(radioGroup: number){
+
+        let prijato = false;
+
+        radio.onReceivedString(function (receivedString) {
+            serial.writeLine(receivedString)
+            prijato = true;
+        })
+        radio.setGroup(radioGroup)
+        serial.redirectToUSB()
+
+        control.runInParallel(function () {
+
+            while (true){
+
+                if (prijato){
+
+                    led.plot(0, 0)
+                    pause(100);
+                    prijato = false;
+                }else{
+
+                    led.unplot(0, 0);
+                    pause(100);
+                }
+            }
+
+        });
+
+        
+        input.onButtonPressed(Button.A, function () {
+            radio.sendString('GOGO');
+        })
+
+        input.onButtonPressed(Button.B, function () {
+            radio.sendString('STOP');
+        })
+    }
+
+}
+NewtonovyZakony.spustOdesilatele(
+10,
+Dimension.Z,
+"Z",
+0,
+0,
+AcceleratorRange.OneG
+)
