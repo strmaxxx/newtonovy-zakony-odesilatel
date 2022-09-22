7faf60bb19f2562b2347b64a918a99bc63a2fe7c
strmaxxx <113727481+strmaxxx@users.noreply.github.com>
2022-09-22 15:04:52 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ namespace NewtonovyZakony{
     //% block="Spustí odeílatele skupina_rádia: %radioGroup, Osa_akcelerometru: %dimension, popis_hodnoty: %popis_hodnoty, rychlost_motoru: %rychlost_motoru, Pauza_mezi_merenimi(ms): %t_pause, Počet hodnot v klouzavém průměru: %avg_count, Rozsah měřených hodnot: %range"
     export function spustOdesilatele(radioGroup: number, dimension: Dimension, popis_hodnoty:string, rychlost_motoru:number, t_pause?: number, avg_count?: number, range?: AcceleratorRange){
 
+        let prubezna_hodnota = 0;
         let hodnota_zrychleni = 0;
         let index = 0
         let pole_hodnot: any[] = [];
@@ -22,28 +23,39 @@ namespace NewtonovyZakony{
                 //if (Math.abs(hodnota - posledni_hodnota) < 10) hodnota = posledni_hodnota;
 
                 posledni_hodnota = hodnota;
-                pole_hodnot[index] = hodnota;
-
-                if (index < avg_count) index++;
-                else pole_hodnot.shift();
 
 
                 if(false){ // klouzavý průměr
+
+                    pole_hodnot[index] = hodnota;
+
+                    if (index >= avg_count) index = 0;
 
                     let suma = 0
                     for (let i = 0; i < pole_hodnot.length; i++) suma += pole_hodnot[i]
                     hodnota_zrychleni = suma / pole_hodnot.length;
 
-                }else{ // lowpass
+                }else if(false){ // lowpass
+
+                    pole_hodnot[index] = hodnota;
+
+                    if (index < avg_count) index++;
+                    else pole_hodnot.shift();
 
                     let smoothing = 10; // koeficient vyhlazení
 
                     let value = pole_hodnot[0]; // start with the first input
-                    for (let i = 1, len = pole_hodnot.length; i < len; i++) {
-                        value += (pole_hodnot[i] - value) / smoothing;
+                    for (let j = 1, len = pole_hodnot.length; j < len; j++) {
+                        value += (pole_hodnot[j] - value) / smoothing;
                     }
 
                     hodnota_zrychleni = value;
+                }else{
+
+                    //let smoothing = 10; // koeficient vyhlazení = avg_count
+                    prubezna_hodnota += (hodnota - prubezna_hodnota) / avg_count;
+                    hodnota_zrychleni = prubezna_hodnota;
+                    
                 }
                 pause(10);
             }
@@ -61,11 +73,14 @@ namespace NewtonovyZakony{
             
             if (receivedString == 'GOGO'){
 
-                for(let i = 10; i > 0; i--){
+                wuKong.setAllMotor(rychlost_motoru, rychlost_motoru);
+                /*
+                for(let k = 10; k > 0; k--){
 
-                    wuKong.setAllMotor(rychlost_motoru/i, rychlost_motoru/i);
+                    wuKong.setAllMotor(rychlost_motoru/k, rychlost_motoru/k);
                     pause(20);
                 }
+                */
 
             }
 
